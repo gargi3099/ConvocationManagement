544020eb126bfd91c77eadc786f6f9dee61094e9
gargi3099 <gargi3099@gmail.com>
2021-04-26 17:03:28 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { AwardsComponent } from './awards/awards.component';
+import { RegisterComponent } from './register/register.component';
+import { ContactusComponent } from './contactus/contactus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ScheduleComponent,
+    AwardsComponent,
+    RegisterComponent,
+    ContactusComponent
   ],
   imports: [
     BrowserModule,
